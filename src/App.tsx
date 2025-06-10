ad7a76114f23ce9670586cd86ca72ea12811ec7b
Davi Lopes Rodrigues <davilopesrdgs@gmail.com>
2025-06-10 06:57:25 -0300

    feat: desenv componente Letter

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,20 @@ import "./global.css";
 import styles from "./app.module.css";
 import { Header } from "./components/Header/Header";
 import { Tip } from "./components/Tip/Tip";
+import { Letter } from "./components/Letter/Letter";
 
 function App() {
   return (
     <div className={styles.container}>
       <Header />
       <Tip />
+      <div className={styles.letter}>
+        <Letter correctGuess="R" />
+        <Letter correctGuess="" />
+        <Letter correctGuess="" />
+        <Letter correctGuess="" />
+        <Letter correctGuess="" />
+      </div>
     </div>
   );
 }
